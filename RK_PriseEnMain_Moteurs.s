d75fcba2f31ab670510eff87ff5c1de26d88f5fd
Ait Hamid Adam <aithamidbusiness@gmail.com>
2022-11-16 16:45:48 +0100

    with led code
    Collision des bumpers qui actionne l'arret des moteurs.

--- RK_PriseEnMain_Moteurs.s
+++ RK_PriseEnMain_Moteurs.s
@@ -5,47 +5,195 @@
 
 		AREA    |.text|, CODE, READONLY
 		ENTRY
-		EXPORT _ main
+		EXPORT	__main
+		
+		;; The IMPORT command specifies that a symbol is defined in a shared object at runtime.
+		IMPORT	MOTEUR_INIT					; initialise les moteurs (configure les pwms + GPIO)
+		
+		IMPORT	MOTEUR_DROIT_ON				; activer le moteur droit
+		IMPORT  MOTEUR_DROIT_OFF			; déactiver le moteur droit
+		IMPORT  MOTEUR_DROIT_AVANT			; moteur droit tourne vers l'avant
+		IMPORT  MOTEUR_DROIT_ARRIERE		; moteur droit tourne vers l'arrière
+		IMPORT  MOTEUR_DROIT_INVERSE		; inverse le sens de rotation du moteur droit
+		
+		IMPORT	MOTEUR_GAUCHE_ON			; activer le moteur gauche
+		IMPORT  MOTEUR_GAUCHE_OFF			; déactiver le moteur gauche
+		IMPORT  MOTEUR_GAUCHE_AVANT			; moteur gauche tourne vers l'avant
+		IMPORT  MOTEUR_GAUCHE_ARRIERE		; moteur gauche tourne vers l'arrière
+		IMPORT  MOTEUR_GAUCHE_INVERSE		; inverse le sens de rotation du moteur gauche
+		
+		; This register controls the clock gating logic in normal Run mode
+SYSCTL_PERIPH_GPIO EQU		0x400FE108	; SYSCTL_RCGC2_R (p291 datasheet de lm3s9b92.pdf)
 
-__main
-; INIT des gpios
-; # et inititalisation du LCD
-	bl LCD_gpio_init # init des gpio pour piloter le LCD
-	bl LCD_init s le cursseur est au debut de la premiere ligne
+; The GPIODATA register is the data register
+GPIO_PORTF_BASE		EQU		0x40025000	; GPIO Port F (APB) base: 0x4002.5000 (p416 datasheet de lm3s9B92.pdf)
 
-	bl cursoff
+; The GPIODATA register is the data register
+GPIO_PORTD_BASE		EQU		0x40007000		; GPIO Port D (APB) base: 0x4000.7000 (p416 datasheet de lm3s9B92.pdf)
 
-		BL RSdr
-		LDR R2, =m1
-loop 	LDR Ri, [R2]
-		CMP Ri, #0x00
-		BEQ floop
-		BL WriteLcD
-		add x2, #1
-		b loop
-
-floop 	BL RSir ; on choisit le registre d'instructions de LCD
-		LDR x1, =0xCO ; pour placer le curseur au debut de la 2ieme R1 = ?????
-		BL WriteLCD ; on envoit cette donnée vers le LCD
-		MOV rl, #DSOus ; il faut attendre >38us
-		BL wait
-		BL RSdr
-		LDR R2, =m2
-loop1	LDR Ri, [R2]
-		CMP Ri, #0x00
-		BEQ floop1
-		BL WriteLcD
-		add r2, #1
-		b loop1
-
-floop1
+; The GPIODATA register is the data register
+GPIO_PORTE_BASE		EQU		0x40024000		; GPIO Port E (APB) base: 0x4002.4000 (p416 datasheet de lm3s9B92.pdf)
 
 
 
-		AREA CONST, DATA,
-m1		dcb "Bonjour !",0
-m2		dcb "& tous
+; configure the corresponding pin to be an output
+; all GPIO pins are inputs by default
+GPIO_O_DIR   		EQU 	0x00000400  ; GPIO Direction (p417 datasheet de lm3s9B92.pdf)
 
-		AREA CONST, DATA, READWRITE
-ms1		space 10
-ms2		space 4
+; The GPIODR2R register is the 2-mA drive control register
+; By default, all GPIO pins have 2-mA drive.
+GPIO_O_DR2R   		EQU 	0x00000500  ; GPIO 2-mA Drive Select (p428 datasheet de lm3s9B92.pdf)
+
+; Digital enable register
+; To use the pin as a digital input or output, the corresponding GPIODEN bit must be set.
+GPIO_O_DEN  		EQU 	0x0000051C  ; GPIO Digital Enable (p437 datasheet de lm3s9B92.pdf)
+
+; Pull_up
+GPIO_I_PUR   		EQU 	0x00000510  ; GPIO Pull-Up (p432 datasheet de lm3s9B92.pdf)
+
+; Broches select
+BROCHE4_5			EQU		0x30		; led1 & led2 sur broche 4 et 5 
+
+BROCHE6				EQU 	0x40		; bouton poussoir 1
+
+BROCHE6_7			EQU 	0xC0		; bouton poussoir 1
+
+BROCHE0_1			EQU		0x03
+
+; blinking frequency
+DUREE   			EQU     0x002FFFFF
+
+
+
+
+__main	
+
+		; ;; Enable the Port F & D peripheral clock 		(p291 datasheet de lm3s9B96.pdf)
+		; ;;									
+		ldr r9, = SYSCTL_PERIPH_GPIO  			;; RCGC2
+        mov r4, #0x00000038  					;; Enable clock sur GPIO D et F où sont branchés les leds (0x28 == 0b101000)
+		; ;;														 									      (GPIO::FEDCBA)
+        str r4, [r9]
+                                                ;; Enable clock sur GPIO E où est branché le bouton poussoir (0x10 == 0b10000)
+                                                ;; (GPIO::FEDCBA)
+
+		; ;; "There must be a delay of 3 system clocks before any GPIO reg. access  (p413 datasheet de lm3s9B92.pdf)
+		nop	   									;; tres tres important....
+		nop	   
+		nop	   									;; pas necessaire en simu ou en debbug step by step...
+	
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION LED
+
+        ldr r9, = GPIO_PORTF_BASE+GPIO_O_DIR    ;; 1 Pin du portF en sortie (broche 4 : 00010000)
+        ldr r4, = BROCHE4_5 	; 0x30 = 0b 0011 0000 
+        str r4, [r9]
+		
+		ldr r9, = GPIO_PORTF_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ldr r4, = BROCHE4_5		
+        str r4, [r9]
+		
+		ldr r9, = GPIO_PORTF_BASE+GPIO_O_DR2R	;; Choix de l'intensité de sortie (2mA)
+        ldr r4, = BROCHE4_5			
+        str r4, [r9]
+		
+		mov r2, #0x000       					;; pour eteindre LED
+     
+		; allumer la led broche 4 (BROCHE4_5)
+		mov r3, #BROCHE4_5		;; Allume LED1&2 portF broche 4&5 : 00110000
+		
+		ldr r9, = GPIO_PORTF_BASE + (BROCHE4_5<<2)  ;; @data Register = @base + (mask<<2) ==> LED1
+			 
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration LED 
+
+		
+		
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION Switcher 1
+		
+		ldr r7, = GPIO_PORTD_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ldr r4, = BROCHE6_7	
+        str r4, [r7]  
+
+		ldr r7, = GPIO_PORTD_BASE+GPIO_I_PUR	;; Pull_up
+        ldr r4, = BROCHE6_7		
+        str r4, [r7]
+
+		ldr r7, = GPIO_PORTD_BASE + (BROCHE6_7<<2)  ;; @data Register = @base + (mask<<2) ==> Switcher
+		
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration Switcher 
+		
+		
+			
+		
+				;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION Bumper 1
+		
+		ldr r8, = GPIO_PORTE_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ldr r4, = BROCHE0_1	
+        str r4, [r8]  
+
+		ldr r8, = GPIO_PORTE_BASE+GPIO_I_PUR	;; Pull_up
+        ldr r4, = BROCHE0_1		
+        str r4, [r8]
+
+		ldr r8, = GPIO_PORTE_BASE + (BROCHE0_1<<2)  ;; @data Register = @base + (mask<<2) ==> Switcher
+		
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration Switcher 
+		
+		
+			
+		
+		
+		
+		
+		
+
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CLIGNOTTEMENT
+
+		str r3, [r9]  							;; Allume LED1&2 portF broche 4&5 : 00110000 (contenu de r3)
+
+;--------------------------------------------------------------------------------------------------------------------
+		;; BL Branchement vers un lien (sous programme)
+
+		; Configure les PWM + GPIO
+		BL	MOTEUR_INIT	   		   
+		
+		; Activer les deux moteurs droit et gauche
+		BL	MOTEUR_DROIT_ON
+		BL	MOTEUR_GAUCHE_ON
+
+
+
+		; Boucle de pilotage des 2 Moteurs (Evalbot tourne sur lui même)
+loop	
+		; Evalbot avance droit devant
+		BL	MOTEUR_DROIT_AVANT	   
+		BL	MOTEUR_GAUCHE_AVANT
+		
+		; Avancement pendant une période (deux WAIT)
+		BL	WAIT	; BL (Branchement vers le lien WAIT); possibilité de retour à la suite avec (BX LR)
+		BL	WAIT
+		
+		; Rotation à droite de l'Evalbot pendant une demi-période (1 seul WAIT)
+		;BL	MOTEUR_DROIT_ARRIERE   ; MOTEUR_DROIT_INVERSE
+		BL	WAIT
+		ldr r10,[r8]
+		
+		CMP r10,#0x00
+		BNE loop
+		
+
+gameover
+		BL MOTEUR_GAUCHE_OFF
+		BL MOTEUR_DROIT_OFF
+		
+;; Boucle d'attante		
+WAIT	ldr r1, =0xAFFFFF
+		
+		
+wait1	subs r1, #1
+        bne wait1
+		
+		;; retour à la suite du lien de branchement
+		BX	LR
+
+		NOP
+        END
